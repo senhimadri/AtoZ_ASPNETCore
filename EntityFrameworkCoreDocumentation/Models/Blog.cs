5f21cec708660c4a9e7d8eb003ab807573b9a7cd
senhimadri <senhimadri.dev@gmail.com>
2024-02-19 14:49:55 +0600

    Onbording Ef Core in Code First Project

---
+++ EntityFrameworkCoreDocumentation/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoreDocumentation.Models;
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; } = string.Empty;
+    public List<Post> Posts { get; } = new();
+}
+
--- GrafanIntegration/Program.cs
+++ GrafanIntegration/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddMetrics();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 builder.Host.UseMetricsWebTracking();
