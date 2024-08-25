80fb659c6e2448f704165546489b43b7f89a0182
senhimadri <senhimadri.dev@gmail.com>
2024-08-25 15:52:40 +0600

    Handle null

--- DependencyInjectionCheck/Program.cs
+++ DependencyInjectionCheck/Program.cs
@@ -14,14 +14,14 @@ using (var scope1 = serviceProvider.CreateScope())
 {
     var service1 = scope1.ServiceProvider.GetService<IServices>();
     var service2 = scope1.ServiceProvider.GetService<IServices>();
-    Console.WriteLine($"Service1 Operation ID: {service1.GetOperationId()}");
-    Console.WriteLine($"Service2 Operation ID: {service2.GetOperationId()}");
+    Console.WriteLine($"Service1 Operation ID: {service1!.GetOperationId()}");
+    Console.WriteLine($"Service2 Operation ID: {service2!.GetOperationId()}");
 }
 
 using (var scope2 = serviceProvider.CreateScope())
 {
     var service3 = scope2.ServiceProvider.GetService<IServices>();
-    Console.WriteLine($"Service3 Operation ID: {service3.GetOperationId()}");
+    Console.WriteLine($"Service3 Operation ID: {service3!.GetOperationId()}");
 }
 
 Console.WriteLine();
@@ -30,8 +30,8 @@ Console.WriteLine();
 Console.WriteLine("Transient:");
 var transientService1 = serviceProvider.GetService<IServices>();
 var transientService2 = serviceProvider.GetService<IServices>();
-Console.WriteLine($"Transient Service1 Operation ID: {transientService1.GetOperationId()}");
-Console.WriteLine($"Transient Service2 Operation ID: {transientService2.GetOperationId()}");
+Console.WriteLine($"Transient Service1 Operation ID: {transientService1!.GetOperationId()}");
+Console.WriteLine($"Transient Service2 Operation ID: {transientService2!.GetOperationId()}");
 
 Console.WriteLine();
 
