bc98d6bb5e02b05213e5e22a41d8644a3d6d82ef
senhimadri <senhimadri.dev@gmail.com>
2025-01-05 18:08:35 +0600

    elastic search project uploades in integrated

--- SimpleApi.ElasticSearch/SimpleApi.ElasticSearch/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SimpleApi.ElasticSearch.Models;
-
-public class User
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-
-}
